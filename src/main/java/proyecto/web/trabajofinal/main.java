78a94c3467c17a080348d416b47ae0dd1543aed5
LeninTF <lenin7777.lt@gmail.com>
2025-06-15 01:07:55 -0500

    APLICATIVO WEB (SPRING BOOT)
    Integracion de Spring boot + Thymeleaf

---
+++ src/main/java/proyecto/web/trabajofinal/main.java
@@ -0,0 +1,13 @@
+package proyecto.web.trabajofinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(main.class, args);
+    }
+
+}
